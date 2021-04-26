f25e8e093feacf712f1e5e6ab12a9a3d69becb4c
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-04-25 21:10:52 -0400

    28.7.2.1. Add More Movies

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: 'movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+  movies = ['The Manchurian Candidate', 'Oceans 8', 'Train to Busan', 'The Farewell', 'Clemency'];
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() {
-   }
+  ngOnInit() {
+  }
 
 }
