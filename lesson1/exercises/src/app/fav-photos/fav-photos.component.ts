6ca1f1f4c675f338aab7c6d3ccd9f76fc190e320
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-04-25 22:11:52 -0400

    28.7.2.2. Complete the fav-photos Component

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo Album';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://america.cgtn.com/wp-content/uploads/2019/08/THE-FAREWELL-COVER.jpg';
+  image3 = 'https://i.ytimg.com/vi/HzUhz2XkFfE/maxresdefault.jpg';
 
   constructor() { }
 
