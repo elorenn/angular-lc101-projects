d703d0db7e6106d7d021a99f02f88ddbecb6f586
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-04-27 06:28:45 -0400

    bonus better than provided solution

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -13,11 +13,24 @@ export class MovieListComponent implements OnInit {
   ngOnInit() {
   }
 
+  // addMovie (newTitle: string) {
+  //   let standardize: string[] = this.movies.map(str => str.toLowerCase().trim());
+  //   if (!standardize.includes(newTitle.toLowerCase().trim()) && newTitle.trim() !== "") {
+  //     this.movies.push(newTitle.trim());
+  //   } 
+  // }
+
   addMovie (newTitle: string) {
+    let errorMsg = '';
     let standardize: string[] = this.movies.map(str => str.toLowerCase().trim());
-    if (!standardize.includes(newTitle.toLowerCase().trim()) && newTitle.trim() !== "") {
+    if(newTitle.trim() === ''){
+      errorMsg = 'Please enter a movie title.';
+    } else if (standardize.includes(newTitle.toLowerCase().trim())) {
+      errorMsg = `${newTitle} is already in your movie list.`;
+    } else {
       this.movies.push(newTitle.trim());
     }
+    return errorMsg;
   }
 
 }
